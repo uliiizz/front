a48a4b653fefaf6e77bee2f936c41db6b03c5fb1
uliiizz <95258373+uliiizz@users.noreply.github.com>
2025-05-18 00:54:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true
+    historyApiFallback: true,
+    port: 80
   },
   base: "/front",
 })
