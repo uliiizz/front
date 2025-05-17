5a8b334ee55ad636290560d0ae902a95869625a9
uliiizz <95258373+uliiizz@users.noreply.github.com>
2025-05-18 01:56:22 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: http://18.175.45.133 ?? process.env.VITE_API_URL ?? 'http://localhost:3000',
+  baseURL: 'http://18.175.45.133' ?? process.env.VITE_API_URL ?? 'http://localhost:3000',
 });
 
 export default api;
